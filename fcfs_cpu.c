d11e0e79e6d09d04657a631909bd146f96ccd05c
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-11-30 03:36:16 +0530

    Update fcfs_cpu.c

--- fcfs_cpu.c
+++ fcfs_cpu.c
@@ -1,38 +1,47 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct fcfs{
+    int pid;
+    int atime;
+    int btime;
+    int ctime;
+    int ttime;
+    int wtime;
+}p[10],temp;
 void main(){
-    int i,j,n,at[10],bt[10],ct[10],tat[10],wt[10],currenttime=0;
-    float total_tat=0,total_wt=0;
-    printf("enter the no of processes:");
+    int n;
+    float atat,awt,tat,wt;
+    printf("---------FCFS------");
+    printf("Enter the number of process");
     scanf("%d",&n);
-    printf("enter the details of each process   ::");
-    
-    for(i=0;i<n;i++){
-        printf("\nenter the arrival time and burst time of p[%d]:",i+1);
-        scanf("\n%d %d",&at[i],&bt[i]);
-    }
-    for(i=0;i<n;i++){
-        if(currenttime>=at[i]){
-            currenttime=currenttime+bt[i];
-            ct[i]=currenttime;
-            tat[i]=ct[i]-at[i];
-            wt[i]=tat[i]-bt[i];
-        }
-        else
-        {
-            currenttime++;
-            i--;
+    for(int i=1;i<=n;i++){
+        printf("Enter the arrival time");
+        scanf("%d",&p[i].atime);
+        printf("Enter the burst time");
+        scanf("%d",&p[i].btime);
+        p[i].pid=i;
+    }p[0].wtime=0;
+    p[0].ctime=0;
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            if(p[i].atime>p[j].atime){
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
         }
     }
-    
-    printf("Process  AT BT CT TAT WT\n");
-    for(i=0;i<n;i++){
-        printf("%d %d %d %d %d \n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
+    for(int i=1;i<=n;i++){
+        p[i].ctime=p[i-1].ctime+p[i].btime;
+        p[i].ttime=p[i].ctime-p[i].atime;
+        p[i].wtime=p[i].ttime-p[i].btime;
+        tat=tat+p[i].ttime;
+        wt=wt+p[i].wtime;
     }
-    for(i=0;i<n;i++){
-        total_tat=total_tat+tat[i];
-        total_wt=total_wt+wt[i];
-    }
-    printf("Average TAT is %.2f\n",total_tat/n);
-     printf("Average WT is %.2f\n",total_wt/n);
-    
-}
+    for(int i=1;i<=n;i++){
+    printf("\n  %d \t %d \t %d \t %d \t %d \t %d \t  \n ",p[i].pid,p[i].atime,p[i].btime,p[i].ctime,p[i].ttime,p[i].wtime);
+  } 
+    printf("\n  TurnAroundTime %f \n  Waiting Time %f\n",tat,wt);
+    printf("\n Average TurnAroundTime %f \n Average Waiting Time %f\n",tat/n,wt/n);
+
+}   
